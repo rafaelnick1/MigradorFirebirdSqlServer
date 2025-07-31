633217a790b9992150ed3d45baf82790b47bc285
rafaelnick1 <nickmarchesini@gmail.com>
2025-07-31 13:49:52 -0300

    Add files via upload

---
+++ MigraFireSQLSever.dpr
@@ -0,0 +1,14 @@
+program MigraFireSQLSever;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.pas' {Migrador};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMigrador, Migrador);
+  Application.Run;
+end.
